03a2ea79693aa2b4e3d112dbd4de1ccbfc44bbfe
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-09-11 13:27:27 -0400

    Create camera skeleton

--- dji/djiCamera.py
+++ dji/djiCamera.py
@@ -1,4 +1,31 @@
+import queue
+import threading
+
+
 class DJICamera:
     def __init__(self):
         self.running = False
         self.frame_counter = 0
+        self.frame_queue = queue.Queue()
+        self.processing_thread = None
+
+    def setup_recording(self):
+        pass
+
+    def start_recording(self):
+        pass
+
+    def setup_stream(self):
+        pass
+
+    def start_stream(self):
+        pass
+
+    def stop_stream(self):
+        pass
+
+    def stop_recording(self):
+        pass
+
+    def download_last_media(self):
+        pass
