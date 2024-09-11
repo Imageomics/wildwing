a3a4e3c8b3a2edd9b25b93acee8ead1cde147578
zhong-al <74470739+zhong-al@users.noreply.github.com>
2024-09-11 14:25:17 -0400

    Implement camera recording

--- dji/djiCamera.py
+++ dji/djiCamera.py
@@ -1,20 +1,27 @@
 import queue
+import shlex
+import signal
+import subprocess
 import threading
 
 
 class DJICamera:
-    def __init__(self, IP_RC):
+    def __init__(self, IP_RC, download='output.mp4'):
         self.running = False
         self.frame_counter = -1
         self.frame_queue = None
         self.processing_thread = None
+        self.download = download
         self.source = f"rtsp://aaa:aaa@{IP_RC}:8554/streaming/live/1"
+        self.record_command = f'ffmpeg -i {self.source} -c copy {self.download}'
+        self.recording_process = None
+        self.streaming_process = None
 
     def setup_recording(self):
         raise NotImplementedError()
 
     def start_recording(self):
-        raise NotImplementedError()
+        self.recording_process = subprocess.Popen(shlex.split(self.record_command))
 
     def setup_stream(self, live_callback=None):
         self.frame_queue = queue.Queue()
@@ -29,7 +36,8 @@ class DJICamera:
         self.processing_thread.join()
 
     def stop_recording(self):
-        raise NotImplementedError()
+        self.recording_process.send_signal(signal.SIGINT)
+        self.recording_process.wait()
 
     def download_last_media(self):
         raise NotImplementedError()
